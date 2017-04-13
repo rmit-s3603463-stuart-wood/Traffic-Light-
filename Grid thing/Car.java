d5e4e316b56730468f5366b1256b72e04de0142e
s3602951 <s3602951@student.rmit.edu.au>
2017-04-13 14:52:53 +1000

    Update Car.java

--- Grid thing/Car.java
+++ Grid thing/Car.java
@@ -1,70 +1,70 @@
-import java.util.Scanner;
-import java.util.InputMismatchException;
-
-
-public class Main
+public class Car
 {
-
-	public static void main(String[] args) throws InputMismatchException
-	{	
-		Scanner keyboard = new Scanner(System.in);
-		
-		int intersectionColumns = 0;
-		int intersectionRows = 0;
-		int frequency = 0;
-		int greenLightTime = 0;
-		
-		while(!(intersectionColumns >= 1 && intersectionColumns <= 10))
-		{
-			System.out.print("Enter the number of columns of intersections:");
-			try
-			{
-				intersectionColumns = keyboard.nextInt();
-				
-				if(!(intersectionColumns >= 1 && intersectionColumns <= 10))
-				{
-					System.out.println("Please enter an integer from 1 to 10\n");
-				}
-			}
-			catch(InputMismatchException e)
-			{
-				System.out.println("Invalid input - enter an integer from 1 to 10\n");
-				keyboard.next();
-			}
-		}
-		
-		while(!(intersectionRows >= 1 && intersectionRows <= 10) | (intersectionColumns * intersectionRows > 10))
-		{
-			System.out.print("\nEnter the number of rows of intersections:");
-			try
-			{
-				intersectionRows = keyboard.nextInt();
-				
-				if(!(intersectionRows >= 1 && intersectionRows <= 10))
-				{
-					System.out.println("Please enter an integer from 1 to 10\n");
-				}
-				else if(intersectionColumns * intersectionRows > 10)
-				{
-					System.out.println("Number of intersections cannot exceed 10,");
-					System.out.println("Enter a smaller number of rows");
-				}
-			}
-			catch(InputMismatchException e)
-			{
-				System.out.println("Invalid input - enter an integer from 1 to 10");
-				keyboard.next();
-			}
-		}
-		
-		Map map = new Map(intersectionColumns, intersectionRows);
-		map.print();
-		
-		Gui test = new Gui(map.getColumns(), map.getRows(), map);
-		//test.testTiles();
-		
-		//test.setVisible(true);
+	public Car(int x, int y, byte direction, byte colour)
+	{
+		this.x = x;
+		this.y = y;
+		this.direction = direction;
+		this.colour = colour;
 	}
 	
+	private int x;
+	private int y;
+	private byte direction;
+	/* 0 - north
+	   1 - east
+	   2 - south
+	   3 - west */
+	private byte colour;
+	/* 0 - red
+	   1 - blue
+	   2 - green
+	   3 - yellow
+	   4 - pink
+	   5 - purple */
 	
+	public int getX()
+	{
+		return this.x;
+	}
+	
+	public int getY()
+	{
+		return this.y;
+	}
+	
+	public byte getDirection()
+	{
+		return this.direction;
+	}
+	
+	public byte getColour()
+	{
+		return this.colour;
+	}
+	
+	public void move(Tile[][] grid)
+	{
+		
+		switch (this.direction)
+		{
+			case 0: this.y--;
+					grid[this.x][this.y].setIsOccupied(true);
+					grid[this.x][this.y + 1].setIsOccupied(false);
+					break;
+			case 1: this.x++;
+					grid[this.x][this.y].setIsOccupied(true);
+					grid[this.x - 1][this.y].setIsOccupied(false);
+					break;
+			case 2: this.y++;
+					grid[this.x][this.y].setIsOccupied(true);
+					grid[this.x][this.y - 1].setIsOccupied(false);
+					break;
+			case 3: this.x--;
+					grid[this.x][this.y].setIsOccupied(true);
+					grid[this.x + 1][this.y].setIsOccupied(false);
+					break;
+			default: break;
+		}
+	}
 }
